d0f49e6bd3217594fd897f30c40c4a686ddebc37
huyycp <baohuyvongoc1@gmail.com>
2024-01-30 23:25:30 +0700

    add cors

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,7 +1,7 @@
 import userService from "../services/UserService.js"
 
 class UserController {
-    static async index(req, res) {
+    static async index(req, res, next) {
         res.send(await userService.getUser())
     }
 }
--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,6 @@
 
 export const port = 4367;
+export const serverURL = "localhost:" + port; 
 export const database = {
     host: 'localhost',
     username: 'root',
